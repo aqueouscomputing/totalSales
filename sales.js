9e64a5a171901fcfd6b24c6af180876da1838a29
aqueouscomputing <aqueouscomputing@gmail.com>
2019-12-11 18:20:49 -0600

    Update sales.js

--- sales.js
+++ sales.js
@@ -1,13 +1,13 @@
 var days = [];
 
 
-days.push (Number(window.prompt("Enter total sales for Monday.")));
-days.push (Number(window.prompt("Enter total sales for Tuesday.")));
-days.push (Number(window.prompt("Enter total sales for Wednesday.")));
-days.push (Number(window.prompt("Enter total sales for Thursday.")));
-days.push (Number(window.prompt("Enter total sales for Friday.")));
-days.push (Number(window.prompt("Enter total sales for Saturday.")));
-days.push (Number(window.prompt("Enter total sales for Sunday.")));
+days.push(Number(window.prompt("Enter total sales for Monday.")));
+days.push(Number(window.prompt("Enter total sales for Tuesday.")));
+days.push(Number(window.prompt("Enter total sales for Wednesday.")));
+days.push(Number(window.prompt("Enter total sales for Thursday.")));
+days.push(Number(window.prompt("Enter total sales for Friday.")));
+days.push(Number(window.prompt("Enter total sales for Saturday.")));
+days.push(Number(window.prompt("Enter total sales for Sunday.")));
 
 var total = 0;
 
